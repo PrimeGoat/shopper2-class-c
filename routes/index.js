ad1d2563391aa7116e992f0ed8b2465b9077ce94
denis.savgir@codeimmersives.com <denis.savgir@codeimmersives.com>
2020-07-08 10:38:15 -0400

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  // res.render('index', { title: 'Express' });
+  console.log('user', req.user);
+  console.log(req.session)
+  res.render('main/home')
+});
+router.get('/logout',(req,res)=>{
+  req.logout();
+  
+  req.session.destroy()
+  return res.redirect('/api/users/login')
+})
+module.exports = router;
